d0201a4d13a616faf8f38d21d13ceee9c60d4163
Rajib Pandit <rpandit@MacBookPro.attlocal.net>
2025-04-11 09:46:40 -0500

    First commit

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
